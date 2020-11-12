854a078f582d2b4b8a9160b21b9b3a62161ce836
Chris <cnewton2@buffalo.edu>
2020-11-12 13:29:44 -0500

    Adding pipelines

--- predict/scripts/dissertation_proposal.py
+++ predict/scripts/dissertation_proposal.py
@@ -38,58 +38,49 @@ X = data[features] # imput features
 # splitting the data
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-# Pipeline
-#model_pipeline = Pipeline([
-#    ('imputer', SimpleImputer(strategy="median")),
-#    ('std_scaler', StandardScaler()),
-#])
-
-# imputing NaNs
-my_imputer = SimpleImputer()
-X_train_imputed = pd.DataFrame(my_imputer.fit_transform(X_train))
-X_test_imputed = pd.DataFrame(my_imputer.transform(X_test))
-
-X_train_imputed.columns = X_train.columns
-X_test_imputed.columns = X_test.columns
-
-# feature scaling
-sc = StandardScaler()
-X_train_sc = sc.fit_transform(X_train_imputed)
-X_test_sc = sc.transform(X_test_imputed)
-
 # linear regression
 def lin_models(X_train, y_train, X_test, y_test, model_name):
-      lin_reg = LinearRegression() 
-      lin_reg.fit(X_train, y_train)
-      preds = lin_reg.predict(X_test)
+      pipe = Pipeline([('imputer', SimpleImputer(strategy="median")),
+          ('std_scaler', StandardScaler()),
+          ('lin_reg', LinearRegression())])
+      pipe.fit(X_train, y_train)
+      preds = pipe.predict(X_test)
       print('\nLinear Regression: %s\n' % model_name)
       print('Mean absolute error: %.2f' % mean_absolute_error(y_test, preds))
       print('Mean squared error: %.2f' % mean_squared_error(y_test, preds))
       print('Coefficient of determination: %.2f' % r2_score(y_test, preds))
 
-lin_models(X_train_sc, y_train, X_test_sc, y_test, "Null Model OLS")
+lin_models(X_train, y_train, X_test, y_test, "Null Model OLS")
 
 # stochastic gradient descent
 def sgd_models(X_train, y_train, X_test, y_test, max_iter, 
         penalty, eta0, model_name):
-        sgd_reg = SGDRegressor(max_iter=max_iter, penalty=penalty, eta0=eta0)
-        sgd_reg.fit(X_train, y_train)
-        preds = sgd_reg.predict(X_test)
-        print('\nStocastic Gradient Descent\n')
+        """
+        """
+        pipe = Pipeline([('imputer', SimpleImputer(strategy="median")),
+           ('std_scaler', StandardScaler()),
+           ('sgd_reg', SGDRegressor(max_iter=max_iter, 
+               penalty=penalty, eta0=eta0))])
+        pipe.fit(X_train, y_train)
+        preds = pipe.predict(X_test)
+        print('\nStocastic Gradient Descent %s\n' % model_name) 
         print('Mean absoulte error: %.2f' % mean_absolute_error(y_test, preds))
-        print('Coefficients: \n', sgd_reg.coef_)
         print('Mean squared error: %.2f' % mean_squared_error(y_test, preds))
         print('Coefficient of determination: %.2f' % r2_score(y_test, preds))
 
-sgd_models(X_train_sc, y_train, X_test_sc, y_test, 
+sgd_models(X_train, y_train, X_test, y_test, 
         500, None, 0.001, "Null Model SGD")
 
 # random forest
 def rf_models(X_train, y_train, X_test, y_test, n_estimators,
       random_state, model_name):
-      rf_reg = RandomForestRegressor(n_estimators=n_estimators,
-            random_state=random_state)
-      rf_reg.fit(X_train, y_train)
+      """
+      """
+      pipe = Pipeline([('imputer', SimpleImputer(strategy="median")),
+        ('std_scaler', StandardScaler()),
+        ('rf', RandomForestRegressor(n_estimators-n_estimators,
+            random_state=random_state))])
+      pipe.fit(X_train, y_train)
       preds = rf_reg.predict(X_test)
       print('\nRandom Forest: %s\n' % model_name)
       print('Mean absolute error: %.2f' % mean_absolute_error(y_test, preds))
@@ -97,7 +88,7 @@ def rf_models(X_train, y_train, X_test, y_test, n_estimators,
       print('Coefficient of determination: %.2f' % r2_score(y_test, preds))
 
 
-rf_models(X_train_sc, y_train, X_test_sc, y_test, 200, 0, "Null Model")
+rf_models(X_train, y_train, X_test, y_test, 200, 0, "Null Model")
 
 # Social Disorganization Model
 
@@ -109,20 +100,7 @@ X = data[features] # imput features
 # splitting the data
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-# imputing NaNs
-my_imputer = SimpleImputer()
-X_train_imputed = pd.DataFrame(my_imputer.fit_transform(X_train))
-X_test_imputed = pd.DataFrame(my_imputer.transform(X_test))
-
-X_train_imputed.columns = X_train.columns
-X_test_imputed.columns = X_test.columns
-
-# feature scaling
-sc = StandardScaler()
-X_train_sc = sc.fit_transform(X_train_imputed)
-X_test_sc = sc.transform(X_test_imputed)
-
-rf_models(X_train_sc, y_train, X_test_sc, y_test, 
+rf_models(X_train, y_train, X_test, y_test, 
       200, 0, "Social Disorganization Model")
 
 # Political Economy Model
@@ -130,25 +108,6 @@ features = ['con_dur', 'peace_dur', 'totalbrds', 'totalosv', 'mtnest',
       'v2x_libdem', 'rgdppc']
 
 X = data[features] # imput features
-
-# splitting the data
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-
-# imputing NaNs
-my_imputer = SimpleImputer()
-X_train_imputed = pd.DataFrame(my_imputer.fit_transform(X_train))
-X_test_imputed = pd.DataFrame(my_imputer.transform(X_test))
-
-X_train_imputed.columns = X_train.columns
-X_test_imputed.columns = X_test.columns
-
-# feature scaling
-sc = StandardScaler()
-X_train_sc = sc.fit_transform(X_train_imputed)
-X_test_sc = sc.transform(X_test_imputed)
-
-rf_models(X_train_sc, y_train, X_test_sc, y_test, 
-      200, 0, "Political Economy Model")
 
 # Organizational model
 
@@ -161,18 +120,5 @@ X = data[features] # imput features
 # splitting the data
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-# imputing NaNs
-my_imputer = SimpleImputer()
-X_train_imputed = pd.DataFrame(my_imputer.fit_transform(X_train))
-X_test_imputed = pd.DataFrame(my_imputer.transform(X_test))
-
-X_train_imputed.columns = X_train.columns
-X_test_imputed.columns = X_test.columns
-
-# feature scaling
-sc = StandardScaler()
-X_train_sc = sc.fit_transform(X_train_imputed)
-X_test_sc = sc.transform(X_test_imputed)
-
-rf_models(X_train_sc, y_train, X_test_sc, y_test, 
+rf_models(X_train, y_train, X_test, y_test, 
       200, 0, "Organizational Model")
